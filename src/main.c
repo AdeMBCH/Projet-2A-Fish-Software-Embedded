2b27227b3c9ced45d3bf3045931ff6066fbb81db
AdeMBCH <153865655+AdeMBCH@users.noreply.github.com>
2025-11-12 14:36:52 +0100

    Test 2 servos

--- src/main.c
+++ src/main.c
@@ -21,6 +21,7 @@ static FrameCodec codec;
 static uint8_t    payload_buf[PAYLOAD_SZ];
 
 static ServoCtrl stail;
+static ServoCtrl stail2;
 
 static MsgRouter mr;
 static RouterTaskArgs rargs;
@@ -32,11 +33,17 @@ static void on_ping(const DecodedFrameMsg *m){
 }
 
  void app_main(void){
-    //Servo
+    // //Servo
     sc_init(&stail, 18);
     st_set_us_limits(&stail.model, 1000,1500,2000);
-    sc_set_profile(&stail, 0.8f, 60.f, 0.f);
+    sc_set_profile(&stail, 1.0f, 30.f, 0.f);
     sc_start(&stail);
+
+    //Test servo 2
+    sc_init(&stail2, 19);
+    st_set_us_limits(&stail2.model, 1000,1500,2000);
+    sc_set_profile(&stail2, 0.1f, 60.f, 0.f);
+    sc_start(&stail2);
 
     //UART
     UartPortCfg cfg={ .uart_num=UART_NUM_2, .tx_gpio=17, .rx_gpio=16, .baud=115200, .rx_timeout_ms=50 };
@@ -54,6 +61,7 @@ static void on_ping(const DecodedFrameMsg *m){
 
     
     // Tâches
+    xTaskCreatePinnedToCore(servo_task, "servo_tasktest", 3072, &stail2, PRIO_CTRL, NULL, SERVOCORE);
     xTaskCreatePinnedToCore(servo_task, "servo_task", 3072, &stail, PRIO_CTRL, NULL, SERVOCORE);
     xTaskCreatePinnedToCore(ul_uart_rx_task,     "ul_rx", 4096, &link, PRIO_UART_RX, NULL, COMCORE);
     xTaskCreatePinnedToCore(ul_frame_decode_task,"ul_dec",4096, &link, PRIO_DEC,     NULL, COMCORE);
